4ea7a5afbd205de1566170858bb6bf482b677111
Your Name <terukimateus@outlook.com>
2023-04-06 13:18:32 -0300

    adicionado navbar mobile, troca estilos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -106,3 +106,22 @@ function reveal() {
   }
 
   //////////////
+
+  function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu')
+
+    if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open')
+      document.querySelector('.icon').src = "assets/images/menu_white_36dp.svg";
+    } else {
+      menuMobile.classList.add('open')
+      document.querySelector('.icon').src = "assets/images/close_white_36dp.svg";
+    }
+  }
+
+  function menuHide() {
+    let menuMobile = document.querySelector('.mobile-menu')
+
+    menuMobile.classList.remove('open')
+    document.querySelector('.icon').src = "assets/images/menu_white_36dp.svg";
+  }
